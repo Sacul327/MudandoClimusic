284937f8e4efd0c941fccdfa61809c9fdf17f2c2
Lucas Zahir Bukato <lbukato@tgv.com.ar>
2018-08-01 15:50:54 -0300

    pojos creados
    termine de crear todos los pojos de las tablas de la base de datos

--- src/org/tgv/pojo/Factura_base.java
+++ src/org/tgv/pojo/Factura_base.java
@@ -1,5 +1,38 @@
 package org.tgv.pojo;
 
-public class Factura_base {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="factura_base")
+public class Factura_base {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_factura;
+	private int empleado_id;
+	public int getId_factura() {
+		return id_factura;
+	}
+	public void setId_factura(int id_factura) {
+		this.id_factura = id_factura;
+	}
+	public int getEmpleado_id() {
+		return empleado_id;
+	}
+	public void setEmpleado_id(int empleado_id) {
+		this.empleado_id = empleado_id;
+	}
+	public Factura_base(int id_factura, int empleado_id) {
+		this.id_factura = id_factura;
+		this.empleado_id = empleado_id;
+	}
+	public Factura_base() {
+	}
+	
+	
+	
 }
--- src/org/tgv/pojo/Proveedor.java
+++ src/org/tgv/pojo/Proveedor.java
@@ -1,5 +1,42 @@
 package org.tgv.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="proveedor")
 public class Proveedor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_proveedor;
+	
+	private String nombre_empresa;
+	
+	public int getId_proveedor() {
+		return id_proveedor;
+	}
+	public void setId_proveedor(int id_proveedor) {
+		this.id_proveedor = id_proveedor;
+	}
+	public String getNombre_empresa() {
+		return nombre_empresa;
+	}
+	public void setNombre_empresa(String nombre_empresa) {
+		this.nombre_empresa = nombre_empresa;
+	}
+	public Proveedor(int id_proveedor, String nombre_empresa) {
+		
+		this.id_proveedor = id_proveedor;
+		this.nombre_empresa = nombre_empresa;
+	}
+	public Proveedor() {
+		
+	}
+	
+	
 
 }
